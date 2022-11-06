72023c234786fa9c2a074b11d5fd3ea69561ed47
Tiago Vargas <tiago.ce10@outlook.com>
2022-11-05 21:35:40 -0300

    feat: get truth table based on single atom

--- test_truth_table_maker.py
+++ test_truth_table_maker.py
@@ -1,2 +1,13 @@
 from truth_table_maker import *
+from modules.formula import *
 
+def test_truth_table_for_single_atom():
+	p = Atom('p')
+
+	table = make_truth_table(atoms=[p], formulas=[p])
+
+	assert table == [
+		['p'],
+		['T'],
+		['F'],
+	]
--- truth_table_maker.py
+++ truth_table_maker.py
@@ -1,1 +1,11 @@
 
+from modules.formula import *
+
+def make_truth_table(atoms: list[Atom], formulas: list[Formula]):
+	atom_name = atoms[0].name
+
+	return [
+		[atom_name],
+		['T'],
+		['F'],
+	]
