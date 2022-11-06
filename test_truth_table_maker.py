5a4a178eb13f04e01b604688a919b93a25d9dce5
Tiago Vargas <tiago.ce10@outlook.com>
2022-11-05 22:12:47 -0300

    feat: get truth table based on two atoms

--- test_truth_table_maker.py
+++ test_truth_table_maker.py
@@ -1,6 +1,7 @@
 from truth_table_maker import *
 from modules.formula import *
 
+
 def test_truth_table_for_single_atom():
 	p = Atom('p')
 
@@ -10,4 +11,20 @@ def test_truth_table_for_single_atom():
 		['p'],
 		['T'],
 		['F'],
+	]
+
+
+def test_truth_table_for_two_atoms():
+	p = Atom('p')
+	q = Atom('q')
+
+	table = make_truth_table(atoms=[p, q], formulas=[p, q])
+
+	assert table == [
+		['p', 'q'],
+
+		['T', 'T'],
+		['T', 'F'],
+		['F', 'T'],
+		['F', 'F'],
 	]
--- truth_table_maker.py
+++ truth_table_maker.py
@@ -1,11 +1,32 @@
 
 from modules.formula import *
 
-def make_truth_table(atoms: list[Atom], formulas: list[Formula]):
-	atom_name = atoms[0].name
 
-	return [
-		[atom_name],
-		['T'],
-		['F'],
-	]
+def make_truth_table(atoms: list[Atom], formulas: list[Formula]) -> list[list[str]]:
+	table = []
+
+	header = [atom.name for atom in atoms]
+
+	table.append(header)
+
+	possibilities = get_possibilities(len(atoms))
+
+	for possibility in possibilities:
+		table.append(possibility)
+
+	return table
+
+
+def get_possibilities(number_of_atoms: int) -> list[list[str]]:
+	if number_of_atoms == 1:
+		return [
+			['T'],
+			['F'],
+		]
+	else:
+		return [
+			['T', 'T'],
+			['T', 'F'],
+			['F', 'T'],
+			['F', 'F'],
+		]
