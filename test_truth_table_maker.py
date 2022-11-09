e41969b1cd87064311b1351e2688a30d2c0073cb
Tiago Vargas <tiago.ce10@outlook.com>
2022-11-09 20:37:09 -0300

    test: remove superfluous tests

--- test_truth_table_maker.py
+++ test_truth_table_maker.py
@@ -2,82 +2,37 @@ from truth_table_maker import *
 from modules.formula import *
 
 
-def test_truth_table_for_single_atom():
-	p = Atom('p')
+class TestAtoms:
+	def test_truth_table_for_single_atom(self):
+		p = Atom('p')
 
-	table = make_truth_table(atoms=[p])
+		table = make_truth_table(atoms=[p])
 
-	assert table == [
-		['p'],
-		['F'],
-		['T'],
-	]
-
-
-def test_truth_table_for_two_atoms():
-	p = Atom('p')
-	q = Atom('q')
-
-	table = make_truth_table(atoms=[p, q])
-
-	assert table == [
-		['p', 'q'],
-
-		['F', 'F'],
-		['F', 'T'],
-		['T', 'F'],
-		['T', 'T'],
-	]
+		assert table == [
+			['p'],
+			['F'],
+			['T'],
+		]
 
 
-def test_truth_table_for_three_atoms():
-	p = Atom('p')
-	q = Atom('q')
-	r = Atom('r')
+	def test_truth_table_for_three_atoms(self):
+		p = Atom('p')
+		q = Atom('q')
+		r = Atom('r')
 
-	table = make_truth_table(atoms=[p, q, r])
+		table = make_truth_table(atoms=[p, q, r])
 
-	assert table == [
-		['p', 'q', 'r'],
+		assert table == [
+			['p', 'q', 'r'],
 
-		['F', 'F', 'F'],
-		['F', 'F', 'T'],
-		['F', 'T', 'F'],
-		['F', 'T', 'T'],
+			['F', 'F', 'F'],
+			['F', 'F', 'T'],
+			['F', 'T', 'F'],
+			['F', 'T', 'T'],
 
-		['T', 'F', 'F'],
-		['T', 'F', 'T'],
-		['T', 'T', 'F'],
-		['T', 'T', 'T'],
-	]
+			['T', 'F', 'F'],
+			['T', 'F', 'T'],
+			['T', 'T', 'F'],
+			['T', 'T', 'T'],
+		]
 
-
-def test_truth_table_for_four_atoms():
-	p = Atom('p')
-	q = Atom('q')
-	r = Atom('r')
-	s = Atom('s')
-
-	table = make_truth_table(atoms=[p, q, r, s])
-
-	assert table == [
-		['p', 'q', 'r', 's'],
-
-		['F', 'F', 'F', 'F'],
-		['F', 'F', 'F', 'T'],
-		['F', 'F', 'T', 'F'],
-		['F', 'F', 'T', 'T'],
-		['F', 'T', 'F', 'F'],
-		['F', 'T', 'F', 'T'],
-		['F', 'T', 'T', 'F'],
-		['F', 'T', 'T', 'T'],
-
-		['T', 'F', 'F', 'F'],
-		['T', 'F', 'F', 'T'],
-		['T', 'F', 'T', 'F'],
-		['T', 'F', 'T', 'T'],
-		['T', 'T', 'F', 'F'],
-		['T', 'T', 'F', 'T'],
-		['T', 'T', 'T', 'F'],
-		['T', 'T', 'T', 'T'],
-	]
