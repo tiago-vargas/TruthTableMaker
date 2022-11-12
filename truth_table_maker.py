18a1761d2377a034f6481d70f309787d7a75367d
Tiago Vargas <tiago.ce10@outlook.com>
2022-11-12 12:41:55 -0300

    refactor: names to avoid misinterpretation
    'truth_value' now represents the old meaning of 'valoration'
    
    'valoration' now represents a tuple (Atom, truth_value)

--- truth_table_maker.py
+++ truth_table_maker.py
@@ -10,17 +10,17 @@ def make_truth_table(atoms: list[Atom], formulas: list[Formula]=[]) -> list[list
 	header = atoms_header + formulas_header
 	table.append(header)
 
-	atoms_valorations = get_all_possible_valorations_for_atoms(len(atoms))
+	atoms_truth_values = get_all_possible_truth_values_for_atoms(len(atoms))
 
-	for atoms_valoration in atoms_valorations:
-		row = get_valoration_of_all_formulas(formulas, atoms_valoration)
+	for atoms_truth_value in atoms_truth_values:
+		row = get_truth_values_of_all_formulas(formulas, atoms_truth_value)
 
 		table.append(row)
 
 	return table
 
 
-def get_all_possible_valorations_for_atoms(number_of_atoms: int) -> list[list[str]]:
+def get_all_possible_truth_values_for_atoms(number_of_atoms: int) -> list[list[str]]:
 	result = []
 
 	binary_numbers = get_binary_numbers_as_strings(number_of_atoms)
@@ -54,43 +54,43 @@ def convert_binary_digits_to_T_or_F(string: str) -> list[str]:
 	return result
 
 
-def get_formula_valoration(formula: Formula, atoms_valoration: list[str]) -> str:
+def get_formula_truth_value(formula: Formula, atoms_truth_value: list[str]) -> str:
 	if isinstance(formula, Not):
-		if atoms_valoration[0] == 'F':
+		if atoms_truth_value[0] == 'F':
 			return 'T'
 		else:
 			return 'F'
 	elif isinstance(formula, And):
-		if atoms_valoration[0] == atoms_valoration[1] == 'T':
+		if atoms_truth_value[0] == atoms_truth_value[1] == 'T':
 			return 'T'
 		else:
 			return 'F'
 	elif isinstance(formula, Or):
-		if atoms_valoration[0] == atoms_valoration[1] == 'F':
+		if atoms_truth_value[0] == atoms_truth_value[1] == 'F':
 			return 'F'
 		else:
 			return 'T'
 	elif isinstance(formula, Implies):
-		if atoms_valoration[0] == 'T' and atoms_valoration[1] == 'F':
+		if atoms_truth_value[0] == 'T' and atoms_truth_value[1] == 'F':
 			return 'F'
 		else:
 			return 'T'
 
 
-def get_valoration_of_all_formulas(formulas: list[Formula], atoms_valoration: list[str]) -> list[str]:
-	formulas_valoration = []
+def get_truth_values_of_all_formulas(formulas: list[Formula], atoms_truth_value: list[str]) -> list[str]:
+	formulas_truth_value = []
 
 	if len(formulas) == 1:
-		formulas_valoration.append(get_formula_valoration(formulas[0], atoms_valoration))
+		formulas_truth_value.append(get_formula_truth_value(formulas[0], atoms_truth_value))
 	elif len(formulas) == 2:
-		formulas_valoration.append(get_formula_valoration(formulas[0], atoms_valoration[0:2]))
-		formulas_valoration.append(get_formula_valoration(formulas[1], atoms_valoration[1:]))
+		formulas_truth_value.append(get_formula_truth_value(formulas[0], atoms_truth_value[0:2]))
+		formulas_truth_value.append(get_formula_truth_value(formulas[1], atoms_truth_value[1:]))
 	elif len(formulas) == 4:
-		formulas_valoration.append(get_formula_valoration(formulas[0], atoms_valoration[0:2]))
-		formulas_valoration.append(get_formula_valoration(formulas[1], atoms_valoration[1:]))
-		formulas_valoration.append(get_formula_valoration(formulas[2], atoms_valoration[::2]))
-		formulas_valoration.append(get_formula_valoration(formulas[3], atoms_valoration[::2]))
+		formulas_truth_value.append(get_formula_truth_value(formulas[0], atoms_truth_value[0:2]))
+		formulas_truth_value.append(get_formula_truth_value(formulas[1], atoms_truth_value[1:]))
+		formulas_truth_value.append(get_formula_truth_value(formulas[2], atoms_truth_value[::2]))
+		formulas_truth_value.append(get_formula_truth_value(formulas[3], atoms_truth_value[::2]))
 
-	row = atoms_valoration + formulas_valoration
+	row = atoms_truth_value + formulas_truth_value
 
 	return row
